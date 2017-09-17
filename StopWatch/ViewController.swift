260709b91c0e76bcb3ccb02cb1272b996c2239b0
Satoshi Komatsu <zlia.6.lj.425@gmail.com>
2017-09-17 21:19:59 +0900

    first

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var ueLabel: UILabel!
+    
+    var count: Float = 0.0
+    var timer: Timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,47 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func start(_ sender: Any) {
+        if !timer.isValid{
+            timer = Timer.scheduledTimer(
+                timeInterval: 0.01,
+                target: self,
+                selector: #selector(self.up),
+                userInfo: nil,
+                repeats: true)
+        }
+    }
 
+    @IBAction func stop(_ sender: Any) {
+//        もしタイマーがvalidならば
+        if timer.isValid{
+//            タイマーをinvalidにします
+            timer.invalidate()
+            self.hantei()
+        }
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        if timer.isValid{
+            count = 0.0
+            
+        }
+    }
+    func up(){
+        count = count + 0.01
+        label.text = String(format: "%.2f", count)
+    }
+    
+    func hantei(){
+        if count >= 9.80 && count <= 10.20{
+            ueLabel.text = "PERFECT"
+        }else if count >= 9.70 && count <= 10.30{
+            ueLabel.text = "GREAT"
+        }else if count >= 9.50 && count <= 10.50{
+            ueLabel.text = "GOOD"
+        }else {
+            ueLabel.text = "BAD"
+        }
+    }
 }
 
